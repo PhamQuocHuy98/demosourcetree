16abe260e8b01f2ad85e77057db0e6eddd646c45
Phạm Quốc Huy <phamquochuy24082013@gmail.com>
2020-06-03 17:40:04 +0700

    init project java

--- src/demojavasourcetree/DemoJavaSourceTree.java
+++ src/demojavasourcetree/DemoJavaSourceTree.java
@@ -3,19 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package demosourcetreejava;
+package demojavasourcetree;
 
 /**
  *
  * @author phamquochuy
  */
-public class DemoSourceTreeJava {
+public class DemoJavaSourceTree {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       
     }
     
 }
